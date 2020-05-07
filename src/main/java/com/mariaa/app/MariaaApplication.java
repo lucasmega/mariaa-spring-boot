559e3ed41bcb56b0b16e397ae54684dcbdbe94d1
Lucas Mega (P) <X191554@prservicos.com.br>
2020-05-07 15:51:30 -0300

    Projeto criado

---
+++ src/main/java/com/mariaa/app/MariaaApplication.java
@@ -0,0 +1,13 @@
+package com.mariaa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariaa/app/MariaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariaa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
