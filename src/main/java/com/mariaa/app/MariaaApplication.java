f1afbd70645eb6d8493f95cb825808da1c350be0
lucasmega <lucas.mega07@gmail.com>
2020-05-09 19:44:10 -0300

    Implementação operação de instanciação

--- src/main/java/com/mariaa/app/MariaaApplication.java
+++ src/main/java/com/mariaa/app/MariaaApplication.java
@@ -1,13 +1,29 @@
 package com.mariaa.app;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mariaa.app.domain.Category;
+import com.mariaa.app.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class MariaaApplication {
+public class MariaaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MariaaApplication.class, args);
 	}
-
+	
+	
+	public void run(String... args) throws Exception {
+		Category domestic = new Category(null, "Domestica");
+		
+		categoryRepository.saveAll(Arrays.asList(domestic));
+	}
 }
