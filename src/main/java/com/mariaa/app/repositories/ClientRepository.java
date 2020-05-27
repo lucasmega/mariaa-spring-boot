94998ee76ea63cbf734bb34e6373b0889341034c
lucasmega <lucas.mega07@gmail.com>
2020-05-26 21:51:44 -0300

    Validação de e-mail já existente - Validação customizada

--- src/main/java/com/mariaa/app/repositories/ClientRepository.java
+++ src/main/java/com/mariaa/app/repositories/ClientRepository.java
@@ -2,10 +2,13 @@ package com.mariaa.app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mariaa.app.domain.Client;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 	
+	@Transactional(readOnly = true)
+	Client findByEmail(String email);
 }
--- src/main/java/com/mariaa/app/services/validation/ClientInsertValidator.java
+++ src/main/java/com/mariaa/app/services/validation/ClientInsertValidator.java
@@ -6,15 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.mariaa.app.domain.enums.CustomerType;
 import com.mariaa.app.dto.CostumerDTO;
+import com.mariaa.app.repositories.ClientRepository;
 import com.mariaa.app.resources.exception.FieldMessage;
 import com.mariaa.app.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClientInsert, CostumerDTO> {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@Override
-	public void initialize(ClientInsert ann) {
-	}
+	public void initialize(ClientInsert ann) { }
 
 	@Override
 	public boolean isValid(CostumerDTO dto, ConstraintValidatorContext context) {
@@ -26,6 +32,10 @@ public class ClientInsertValidator implements ConstraintValidator<ClientInsert, 
 
 		if (dto.getCustomerType().equals(CustomerType.LEGAL_PERSON.getCode()) && !BR.isValidCNPJ(dto.getDocument())) {
 			list.add(new FieldMessage("document", "CNPJ inválido!"));
+		}
+		
+		if (clientRepository.findByEmail(dto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "E-mail já existe!"));
 		}
 
 		for (FieldMessage e : list) {
